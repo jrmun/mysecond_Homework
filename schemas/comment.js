e28672976cee752251dcfe9cbf949b1e790ff98e
Lee <jrmun123@gmail.com>
2023-06-14 19:56:48 +0900

    댓글 기능 구현

---
+++ schemas/comment.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const commentSchemas = new mongoose.Schema({
+  cmtId: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  postId: {
+    type: Number,
+    required: true,
+  },
+  cmtName: {
+    type: String,
+    required: true,
+  },
+  cmtpassword: {
+    type: Number,
+    required: true,
+  },
+  cmtSubstance: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("comment", commentSchemas);
