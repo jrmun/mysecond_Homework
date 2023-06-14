a16c68682ebdca605c5f9a4b32076c1354b4cd9e
Lee <jrmun123@gmail.com>
2023-06-14 16:17:02 +0900

    index 라우터 작성

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000;
 const connect = require("./schemas/index.js");
 connect();
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(port, "포트로 서버가 열렸어요!");
+});
