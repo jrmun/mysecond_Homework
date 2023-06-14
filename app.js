8a216283e5f07bdddf3239939ca2bd12de6864ac
Lee <jrmun123@gmail.com>
2023-06-14 17:32:46 +0900

    조회, 생성까지 완료

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const indexRouter = require("./routes/index.js");
+const postsRouter = require("./routes/posts.js");
 const connect = require("./schemas/index.js");
+
 connect();
 
 app.use(express.json());
+
+app.use("/api", indexRouter, postsRouter);
 
 app.listen(port, () => {
   console.log(port, "포트로 서버가 열렸어요!");
---
+++ routes/posts.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const posts = require("../schemas/post.js");
+
+router.get("/posts/:postId", async (req, res) => {
+  const { postId } = req.params;
+  try {
+    const postList = await posts.find({ postId: postId });
+    res.status(200).json({ delail: postList });
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+router.post("/posts", async (req, res) => {
+  const { postId, postTitle, name, password, postContent } = req.body;
+  const existsPost = await posts.find({ postId: Number(postId) });
+  if (existsPost.length) {
+    return res
+      .status(400)
+      .json({ success: false, errorMessage: "이미 존재하는 postId입니다." });
+  }
+  const createdPost = await posts.create({
+    postId,
+    postTitle,
+    name,
+    password,
+    postContent,
+  });
+
+  res.json({ posts: createdPost });
+});
+
+module.exports = router;
