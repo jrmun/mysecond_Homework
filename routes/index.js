47cd321e9fe251fab36c8f2df69706acbc2cc5a4
Lee <jrmun123@gmail.com>
2023-06-16 15:31:18 +0900

    비밀번호 해쉬화

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const posts = require("../schemas/post.js");
 
+router.get("/", async (req, res) => {
+  res.json("게시글 확인 : posts");
+});
+
 router.get("/posts", async (req, res) => {
   try {
     const postList = await posts.find({});
